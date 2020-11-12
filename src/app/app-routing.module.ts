62c9e8c89a6d2ebbeaac5be407f69cc4cba1fc50
Renan Carrillo <renan@MacBook-Pro-de-Renan.local>
2020-11-11 17:31:55 -0700

    Se agregaron rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'lista',
+    loadChildren: () => import('./lista/lista.module').then( m => m.ListaPageModule)
   }
 ];
 @NgModule({
